abd37355e1f63e52daa1dcd854ab0f75414c351f
AgungMarvin <agungmarvin38@gmail.com>
2026-05-10 22:14:41 +0800

    Update 02_jawaban_manual_agung.cpp

--- 02-linked-List/02_jawaban_manual_agung.cpp
+++ 02-linked-List/02_jawaban_manual_agung.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
 
+// Nama: Agung Marvin - Linked List Manual
 struct Node {
     int val;
     Node *next;
+    Node(int x) : val(x), next(NULL) {}
 };
 
 Node* balikList(Node* head) {
@@ -15,4 +17,23 @@ Node* balikList(Node* head) {
         curr = next;
     }
     return prev;
+}
+
+int main() {
+    // Membuat list manual 1->2->3->4->5
+    Node* h = new Node(1);
+    h->next = new Node(2);
+    h->next->next = new Node(3);
+    h->next->next->next = new Node(4);
+    h->next->next->next->next = new Node(5);
+
+    // Proses balik
+    h = balikList(h);
+
+    // Cetak hasil
+    while(h) { 
+        cout << h->val << " "; 
+        h = h->next; 
+    }
+    return 0;
 }
