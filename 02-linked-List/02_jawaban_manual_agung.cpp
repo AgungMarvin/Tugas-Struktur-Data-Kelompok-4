7b5e7912c00e601e1dcae4371c5b5d4d63a208c6
AgungMarvin <agungmarvin38@gmail.com>
2026-05-10 21:25:57 +0800

    Update 02_jawaban_manual_agung.cpp

--- 02-linked-List/02_jawaban_manual_agung.cpp
+++ 02-linked-List/02_jawaban_manual_agung.cpp
@@ -1,69 +1,18 @@
 #include <iostream>
-
 using namespace std;
 
-/**
- * Nama: AgungMarvin
- * Tugas: Implementasi Reverse Linked List
- */
-
-// Struktur Node standar
 struct Node {
-    int data;
-    Node* next;
-    Node(int val) {
-        data = val;
-        next = NULL;
-    }
+    int val;
+    Node *next;
 };
 
-class Solusi {
-public:
-    // Fungsi untuk membalikkan urutan list
-    Node* balikkanList(Node* head) {
-        Node* sebelumnya = NULL;
-        Node* sekarang = head;
-        Node* berikutnya = NULL;
-
-        while (sekarang != NULL) {
-            // Simpan alamat node selanjutnya dulu
-            berikutnya = sekarang->next;
-
-            // Balikkan arah pointer ke node sebelumnya
-            sekarang->next = sebelumnya;
-
-            // Geser posisi ke node berikutnya
-            sebelumnya = sekarang;
-            sekarang = berikutnya;
-        }
-
-        return sebelumnya;
+Node* balikList(Node* head) {
+    Node *prev = NULL, *curr = head, *next = NULL;
+    while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
     }
-};
-
-// Untuk menampilkan isi list di terminal
-void cetakList(Node* node) {
-    while (node != NULL) {
-        cout << node->data << " -> ";
-        node = node->next;
-    }
-    cout << "NULL" << endl;
-}
-
-int main() {
-    // Contoh input: 1 -> 2 -> 3
-    Node* kepala = new Node(1);
-    kepala->next = new Node(2);
-    kepala->next->next = new Node(3);
-
-    cout << "List Awal: ";
-    cetakList(kepala);
-
-    Solusi s;
-    kepala = s.balikkanList(kepala);
-
-    cout << "List Setelah Dibalik: ";
-    cetakList(kepala);
-
-    return 0;
+    return prev;
 }
